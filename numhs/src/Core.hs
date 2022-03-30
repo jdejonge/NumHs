c904334e4328ae0af2d5bdb121326387d15bfa22
Joes <j.dejonge@students.uu.nl>
2022-03-30 16:17:19 +0200

    Testable

--- numhs/src/Core.hs
+++ numhs/src/Core.hs
@@ -5,25 +5,24 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-
+For using in GHCI you have to use:
+:set -XDataKinds
+:set -XGADTs
+:set -XNoStarIsType
+:set -XPolyKinds
+:set -XTypeFamilies
+:set -XTypeOperators
+:set -XUndecidableInstances
+:set -XScopedTypeVariables
+-}
 
 {-|
 Module      : Core
 Description : This module describes the core data types and functions of NumHS.
 -}
-module Core(
-    Error(..),
-    Tensor(Dense, Sparse),
-    vector,
-    fromList,
-    shape,
-    reshape,
-    (|@|),
-    (|:|),
-    (|#|),
-    ListN(Nil, Cons),
-    toIndex,
-    toIndexHelper,
-) where
+module Core where
 
 import qualified Data.List as L
 import qualified Data.List.Split as LS
@@ -32,7 +31,7 @@ import GHC.Base (undefined)
 -- |Tensor error, returned by functions that might fail depending on the shape of its argument(s).
 newtype Error = Error String deriving(Show)
 
-data Interval = Between Int Int | Or Interval Interval
+data Interval = Between Nat Nat | Or Interval Interval
 
 instance Show Interval where
     show (Between lower upper) = show lower ++ ":" ++ show upper
@@ -45,8 +44,15 @@ data Nat where
 
 instance Eq Nat where
     Zero == Zero = True
+    Zero == Succ x = False
     Succ x == Zero = False
     Succ x == Succ y = x == y
+
+instance Ord Nat where
+    Zero <= _ = True 
+    Succ x <= Zero = False
+    Succ x <= Succ y = x <= y
+
 
 -- | A list of type a and of length n
 data ListN (dim :: Nat) a where
@@ -56,12 +62,12 @@ infixr 5 `Cons`
 
 instance (Show a) => Show (ListN n a) where
     show Nil = "Nil"
-    show (Cons xs@Nil x) = "Cons " ++ show xs ++ " " ++ show x
-    show (Cons xs x) = "Cons " ++ show x  ++ " (" ++ show x ++ ")"
+    show (Cons x xs@Nil) = "Cons " ++ show x ++ " " ++ show xs
+    show (Cons x xs) = "Cons " ++ show x  ++ " (" ++ show xs ++ ")"
 
 instance Foldable (ListN a) where
     foldr f b Nil = b
-    foldr f b (Cons xs x) = f x (foldr f b xs)
+    foldr f b (Cons x xs) = f x (foldr f b xs)
 
 fromInt :: (Num t, Ord t) => t -> Nat
 fromInt 0   = Zero
@@ -80,16 +86,23 @@ inInterval (Between lower upper) n = lower <= n && n < upper
 inInterval (Or left right) n = inInterval left n || inInterval right n
 
 type family Plus m n where
-  Plus (Succ m) n = Plus m (Succ n)
-  Plus Zero n = n
+    Plus (Succ m) n = Plus m (Succ n)
+    Plus Zero n = n
 
 type family Times m n where
-  Times (Succ m) n = Plus n (Times m n)
-  Times Zero n = Zero
+    Times (Succ m) n = Plus n (Times m n)
+    Times Zero n = Zero
 
 type family Product (dims) where
-  Product '[] = Succ Zero
-  Product (m : ns) = Times m (Product ns)
+    Product '[] = Succ Zero
+    Product (m : ns) = Times m (Product ns)
+
+type family Length dims where
+    Length '[] = Zero
+    Length (m : ms) = Succ (Length ms)
+
+data SameProduct (dims1 :: [Nat]) (dims2 :: [Nat]) where
+    Same :: (Product dims1 ~ d1, Product dims2 ~ d2, d1 ~ d2) => SameProduct dims1 dims2
 
 -- |Main Tensor datatype.
 data Tensor (dims :: [Nat]) a where
@@ -101,7 +114,7 @@ t1 = Dense (1 `Cons` 2 `Cons` 3 `Cons` 4 `Cons` 5 `Cons` 6 `Cons` Nil)
 
 getCompleteDimension :: ListN n Int-> Int
 getCompleteDimension Nil = 0
-getCompleteDimension (Cons xs x) = x * getCompleteDimension xs
+getCompleteDimension (Cons x xs) = x * getCompleteDimension xs
 
 intToNat :: Int -> Nat
 intToNat 0 = Zero
@@ -111,35 +124,31 @@ intToNat x  | x > 0 = Succ (intToNat $ x - 1)
 
 -- |Returns the shape of the `Tensor`.
 shape :: Tensor n a-- ^The `Tensor` we want to know the shape of.
-        -> dims -- ^Returns the shape of the argument Tensor.
-shape (Dense _ s) = s
+        -> [Nat] -- ^Returns the shape of the argument Tensor.
+shape (Dense _ :: Tensor l m) = undefined
 shape Sparse = undefined
 
 instance (Show a) => Show (Tensor n a) where
-    show (Dense xs ls) = "Dense (" ++ show xs ++ ")  (" ++ show ls ++ ")"
+    show (Dense ls) = "Dense (" ++ show ls ++ ")"
     --show (Dense xs (s:shape)) = "[" ++ L.intercalate ", " (map (\x -> show $ let (Right vec) = fromList x shape in vec) (LS.chunksOf (length xs  `div` s) xs)) ++ "]"
     show Sparse = undefined
 
 (!!) :: ListN n a -> Nat -> a
 Nil !! _ = error "There is no element left to get."
-(Cons xs x) !! Zero = x
-(Cons xs x) !! (Succ n) = xs Core.!! n
-
-ls :: ListN n a -> ListN n a -> a
-ls = undefined
+(Cons x xs) !! Zero = x
+(Cons x xs) !! (Succ n) = xs Core.!! n
 
 -- |Reshaping Tensors.
-reshape :: (Product lm ~ l, Product (ns :: [Nat]) ~ n, l ~ n )=>
-        Tensor ns a -- ^`Tensor` to be reshaped. 
-        -> ls -- ^The new shape. Its product should equal the product of the old shape.
-        -> Either Error (Tensor lm a) -- ^Returns the `Tensor` with the new shape on succes.
-reshape (Dense d shape) newshape    | product shape == product newshape = Right $ Dense d newshape
-                                    | otherwise = Left $ Error $ "Cannot reshape Tensor of shape " ++ show shape ++ " to new shape " ++ show newshape ++ "."
-reshape Sparse newshape = undefined
+reshape :: (Product ls1 ~ l1, Product ls2 ~ l2, l1 ~ l2 ) =>
+        Tensor ls1 a -- ^`Tensor` to be reshaped. 
+        -> SameProduct ls1 ls2 -- ^The new shape. Its product should equal the product of the old shape.
+        -> Tensor ls2 a -- ^Returns the `Tensor` with the new shape on succes.
+reshape (Dense shape) _ = Dense shape
+reshape Sparse _ = undefined
 
 lengthListN :: ListN n a -> Nat
 lengthListN Nil = Zero
-lengthListN (Cons xs x) = Succ (lengthListN xs)
+lengthListN (Cons x xs) = Succ (lengthListN xs)
 
 natToInt :: Nat -> Int
 natToInt Zero = 0
@@ -147,18 +156,23 @@ natToInt (Succ x) = 1 + natToInt x
 
 
 -- |Creates a vector from a given list.
-vector :: (Product l ~ m, length l ~ 1) =>
+vector :: (Product l ~ m, Length l ~ Succ Zero) =>
         ListN m a -- ^The list containing the elements of the vector.
-        -> Tensor l a-- ^Returns a 1D `Tensor`.
-vector Nil = Dense Nil (Cons Nil 0)
-vector (Cons xs x) = Dense (Cons xs x) (Cons Nil (natToInt $ lengthListN $ Cons xs x))
+        -> Tensor l a -- ^Returns a 1D `Tensor`.
+vector Nil = Dense Nil
+vector l@(Cons _ _) = Dense l
 
 -- |Creates a `Tensor` of the given shape from a list of elements.
-fromList :: ListN m a -- ^A flat list containing the elements of the tensor.
-        -> ListN n Int -- ^The shape of the tensor, such that @product shape = length list@
-        -> Either Error (Tensor m n a) -- ^Returns a `Tensor` of the given shape holding the data on success.
-fromList as shape   | product shape == length as = Right $ Dense as shape
-                    | otherwise = Left $ Error $ "List with " ++ show (length as) ++ " elements cannot be casted to a square Tensor of shape " ++ show shape ++ "."
+fromListWithSP :: (Product l1 ~ l, Product l2 ~ n, l ~ n ) =>
+        ListN n a -- ^A flat list containing the elements of the tensor.
+        -> SameProduct l1 l2 -- ^The shape of the tensor, such that @product shape = length list@
+        -> Tensor l2 a -- ^Returns a `Tensor` of the given shape holding the data on success.
+fromListWithSP as _   = Dense as
+
+fromList :: (Product l1 ~ n, l ~ n ) =>
+        ListN n a -- ^A flat list containing the elements of the tensor.
+        -> Tensor l1 a -- ^Returns a `Tensor` of the given shape holding the data on success.
+fromList = Dense
 
 -- |Indexing Tensors. Retrieves a single element at the specified index.
 --
@@ -170,33 +184,34 @@ fromList as shape   | product shape == length as = Right $ Dense as shape
 -- >>> [5, 9, 6, 7] |@| [2]
 -- 6
 
-toIndexHelper :: ListN (Succ n) Int -> ListN (Succ n) Int -> Int -> Either Error Int
-toIndexHelper (Cons Nil x) (Cons Nil y) d   | y < x = Right (d * y)
-                                            | otherwise = Left $ Error $ "coordinate " ++ show y ++ " is too large for dimension " ++ show x
-toIndexHelper (Cons xs@(Cons _ _) x) (Cons ys@(Cons _ _) y) d
-    | y < x =
-        case toIndexHelper xs ys (d * x) of
-            Left msg -> Left msg
-            Right rest -> Right (d * y + rest)
-    | otherwise = Left $ Error $ "coordinate " ++ show y ++ " is too large for dimension " ++ show x
+-- toIndexHelper :: ListN (Succ n) Int -> ListN (Succ n) Int -> Int -> Either Error Int
+-- toIndexHelper (Cons Nil x) (Cons Nil y) d   | y < x = Right (d * y)
+--                                             | otherwise = Left $ Error $ "coordinate " ++ show y ++ " is too large for dimension " ++ show x
+-- toIndexHelper (Cons xs@(Cons _ _) x) (Cons ys@(Cons _ _) y) d
+--     | y < x =
+--         case toIndexHelper xs ys (d * x) of
+--             Left msg -> Left msg
+--             Right rest -> Right (d * y + rest)
+--     | otherwise = Left $ Error $ "coordinate " ++ show y ++ " is too large for dimension " ++ show x
 
--- | toIndex :: size -> indices -> current dimension level -> index
-toIndex :: ListN (Succ n) Int -> ListN (Succ n) Int -> Either Error Nat
-toIndex x y =
-    case toIndexHelper x y 1 of
-        Left msg -> Left msg
-        Right x -> Right (fromInt x)
+-- -- | toIndex :: size -> indices -> current dimension level -> index
+-- toIndex :: ListN (Succ n) Int -> ListN (Succ n) Int -> Either Error Nat
+-- toIndex x y =
+--     case toIndexHelper x y 1 of
+--         Left msg -> Left msg
+--         Right x -> Right (fromInt x)
 
-(|@|) :: Tensor m n a
-        -> ListN n Int -- ^List of indices, one integer for each dimension.
+(|@|) :: Tensor n a
+        -> [Nat] -- ^List of indices, one integer for each dimension.
         -> Either Error a -- ^Returns an element of the Tensor on succes.
+_ |@| _ = undefined
 -- This assumes that there is no `Tensor` with shape [], which should be enforced by the functions used to create tensors.
-(Dense Nil _) |@| _ = Left (Error "The tensor doesn't contain any elements.")
-(Dense xs Nil) |@| _ = Left (Error "The tensor doesn't have any dimension.")
-(Dense xs shape@(Cons _ _)) |@| index@(Cons _ _) = case toIndex shape index of
-    Left msg -> Left msg
-    Right n -> Right $ xs Core.!! n
-Sparse |@| _ = undefined
+-- (Dense Nil) |@| _ = Left (Error "The tensor doesn't contain any elements.")
+-- (Dense xs Nil) |@| _ = Left (Error "The tensor doesn't have any dimension.")
+-- (Dense xs shape@(Cons _ _)) |@| index@(Cons _ _) = case toIndex shape index of
+--     Left msg -> Left msg
+--     Right n -> Right $ xs Core.!! n
+-- Sparse |@| _ = undefined
 
 -- |Slicing allows for taking a contiguous subsection of a `Tensor`.
 --
@@ -225,7 +240,7 @@ Sparse |@| _ = undefined
 -- helperFilter :: ListN n a -> ListN n Interval -> SNat m -> ListN m a
 -- helperFilter Nil _ = Nil
 
-(|#|) :: Tensor n a -> ListN n Interval -> Either Error (Tensor q a)
+(|#|) :: (Product n ~ m) => Tensor n a -> ListN m Interval -> Either Error (Tensor q a)
 (|#|) = undefined
 
 
