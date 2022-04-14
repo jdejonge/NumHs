0cd2bbb89623a48ad88a6a81579e618cd5cd693a
Joes <j.dejonge@students.uu.nl>
2022-04-14 16:58:52 +0200

    More Sparse functions

--- numhs/app/Main.hs
+++ numhs/app/Main.hs
@@ -6,12 +6,13 @@ import LinAlg
 
 main :: IO ()
 main = do
+    putStrLn ""
     -- Indexing
-    let (Right vec) = fromList [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27] [3, 3, 3] -- Creating a 3D array of shape [2, 2, 2]
-    putStrLn $ "3D tensor: " ++ show vec ++ ". Indexed at [0, 1, 1] = " ++ show (vec |@| [0, 1, 1])
+    -- let (Right vec) = fromList [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27] [3, 3, 3] -- Creating a 3D array of shape [2, 2, 2]
+    -- putStrLn $ "3D tensor: " ++ show vec ++ ". Indexed at [0, 1, 1] = " ++ show (vec |@| [0, 1, 1])
 
-    let idtensor = idTensor 3 3
-    putStrLn $ "ID tensor: " ++ show idtensor ++ "."
+    -- let idtensor = idTensor 3 3
+    -- putStrLn $ "ID tensor: " ++ show idtensor ++ "."
 
     --let (Right vec1) = fromList [1, 4, 2, 5, 3, 6] [2, 3]
     --let (Right vec2) = fromList [7, 9, 11, 8, 10, 12] [3, 2]
