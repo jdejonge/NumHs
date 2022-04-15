f727ea5cd4ffa49d5c095cd065a512f861d902df
Kieran van Gaalen <kieran.van.gaalen@casema.nl>
2022-04-15 19:58:13 +0200

    Fix dot product for default

--- numhs/src/LinAlg.hs
+++ numhs/src/LinAlg.hs
@@ -18,6 +18,7 @@ import Core
 import Math
 import Data.Maybe
 import Data.Either
+import Data.Default
 
 addTensor :: (Num a) => Tensor a -> Tensor a -> Either Error (Tensor a)
 addTensor (Dense v1 s1) (Dense v2 s2) | s1 == s2 = Right $ Dense (zipWith (+) v1 v2) s1
@@ -40,7 +41,7 @@ addDummyElement xs dummy i = x ++ dummy : y
     where (x,y) = splitAt i xs
 
 -- |Gets a dimension vector given a dimension and coordinates on that dimension.
-getDim :: Tensor a -- ^`Tensor` for which the dimension should be found.
+getDim :: Default a => Tensor a -- ^`Tensor` for which the dimension should be found.
         -> Int -- ^Dimension to be gotten.
         -> [Int] -- ^Location of dimension to be gotten, should be a value for every element in shape except for the dimension.
         -> Either Error [a] --Dimension
@@ -54,18 +55,18 @@ getDim t@(Dense v s) x c | length maybeList == length noMaybeList = Right noMayb
                         where y = t |@| replaceElement ls x1 i
 getDim _ _ _ = undefined
                         
-getRow2D :: (Num a) => Tensor a  -> Int -> Either Error [a]
+getRow2D :: (Num a, Default a) => Tensor a  -> Int -> Either Error [a]
 getRow2D t@(Dense xs [d1, d2]) r = getDim t 0 [r]
 getRow2D _ _ = Left $ Error "Only defined for 2 dimensional matrices. Use getDim instead."
 
-getColumn2D :: (Num a) => Tensor a -> Int -> Either Error [a]
+getColumn2D :: (Num a, Default a) => Tensor a -> Int -> Either Error [a]
 getColumn2D t@(Dense x [d1, d2]) c = getDim t 1 [c]
 getColumn2D _ _ = Left $ Error "Only defined for 2 dimensional matrices. Use getDim instead."
 
 dotVector :: (Num a) => [a] -> [a] -> a
 dotVector x y = sum $ zipWith (*) x y
 
-tensorDotIndex :: (Num a) => Tensor a -> Tensor a -> Int -> Int -> a
+tensorDotIndex :: (Num a, Default a) => Tensor a -> Tensor a -> Int -> Int -> a
 tensorDotIndex t1 t2 row col = dotVector rowVec colVec
   where (Right rowVec) = getRow2D t1 row
         (Right colVec) = getColumn2D t2 col
@@ -73,7 +74,7 @@ tensorDotIndex t1 t2 row col = dotVector rowVec colVec
 inner :: (Num a) => Tensor a -> Tensor a -> Tensor a
 inner = undefined
 
-matmul :: (Num a) => Tensor a -> Tensor a -> Either Error (Tensor a)
+matmul :: (Num a, Default a) => Tensor a -> Tensor a -> Either Error (Tensor a)
 matmul t1@(Dense v1 s1@[s11, s12]) t2@(Dense v2 s2@[s21, s22]) | s11 == s22 = Right $ Dense (loopOverCol s12 s21 0) [s12, s21]
                                                          | otherwise = Left $ error $ "shapes " ++ show s1 ++ " and " ++ show s2 ++ " are not compatible."
     where loopOverRow maxRow curCol curRow | curRow == maxRow = []
